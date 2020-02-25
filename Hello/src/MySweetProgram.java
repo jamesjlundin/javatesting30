c28ce709c8dbe4ff5eae68e135f4c3544b050493
The Code Diver <46363396+thecodediver@users.noreply.github.com>
2020-02-25 11:25:54 -0500

    building a stack

--- Hello/src/MySweetProgram.java
+++ Hello/src/MySweetProgram.java
@@ -4,19 +4,15 @@ import java.util.*;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(new File("student.txt"));
-		List<String> students = new ArrayList<String>();
-		while(scanner.hasNextLine()) {
-			students.add(scanner.nextLine());
-		}
-		
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("Name: " + students.get(i));
-		}
-		
-		scanner.close();
-		
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Mark");
+		names.push("Sue");
+		names.push("Tim");
+
+		System.out.println(names.poll());
+		System.out.println(names.poll());
+		System.out.println(names.poll());
 	}
 }
