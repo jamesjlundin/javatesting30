49978fec096359e8cea20804f29e337c821bf3bf
The Code Diver <46363396+thecodediver@users.noreply.github.com>
2020-02-25 10:42:51 -0500

    read input from file

--- Hello/src/MySweetProgram.java
+++ Hello/src/MySweetProgram.java
@@ -1,14 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		//Scanner scanner = new Scanner(System.in);
-		//String s = scanner.nextLine();
-		int x = 5;
-		System.out.println(x);
-		System.out.println("Woot it pushed!");
+		Scanner scanner = new Scanner(new File("student.txt"));
+		List<String> students = new ArrayList<String>();
+		while(scanner.hasNextLine()) {
+			students.add(scanner.nextLine());
+		}
+		
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println("Name: " + students.get(i));
+		}
+		
+		scanner.close();
+		
 	}
 }
